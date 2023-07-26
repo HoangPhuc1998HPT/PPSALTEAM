7f541366c40d1078d49d7d4078f6fee3d840b172
UIT-22730078 <139780226+Lam-22730078@users.noreply.github.com>
2023-07-26 19:33:48 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,12 +12,63 @@ using namespace std;
 int main()
 {
 
+        bool GameOver = false ;
+        int toadox [Max] ;
+        int toadoy [Max] ;
+
+// xay tuong
+    void VeTuong()
+{
+        SetColor(1);
+    VeTuongTren();
+    VeTuongDuoi();
+    VeTuongtrai();
+    VeTuongphai() ;
+        SetColor(9) ;
+
+}
 
 
-
-
-
-
+void VeTuongTren()
+{
+     int x = 10 , y = 1 ;
+    while (x <= 100)
+    {
+    gotoXY(x,y) ;
+    cout << "*" ;
+    x++;
+}
+}
+void VeTuongDuoi()
+{
+     int x = 10 , y = 28 ;
+    while (x < 100)
+    {
+    gotoXY(x,y) ;
+    cout << "*" ;
+    x++;
+}
+}
+void VeTuongtrai()
+{
+     int x = 10 , y = 1 ;
+    while (y <= 28)
+    {
+    gotoXY(x,y) ;
+    cout << "*" ;
+    y++;
+}
+}
+void VeTuongphai()
+{
+     int x = 100 , y = 1 ;
+    while (y <= 28)
+    {
+    gotoXY(x,y) ;
+    cout << "*" ;
+    y++;
+}
+}
 
 
 
