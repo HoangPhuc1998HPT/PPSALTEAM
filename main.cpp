f625c60702c9e78c4d31237b4e8c2df5592e8a24
UIT-22730089 <139891428+UIT-22730089@users.noreply.github.com>
2023-07-26 20:56:23 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,41 @@
 #include <chrono>
 #include <conio.h>
 #include <thread>
-
+#include <time.h>
+#define Max 99
+#include <windows.h>
+#include <colordlg.h>
 
 
 using namespace std;
+int lenghtsnake = 6 ;
+//SV1 vẽ tường + khai báo main - Lâm
+void VeTuongTren();
+void VeTuongDuoi();
+void VeTuongtrai();
+void VeTuongphai();
+void VeTuong();
 
+//SV2 Phát
+void Khoitaotoadoran(int toadox[], int toadoy[]);
+void VeRan (int toadox[] , int toadoy[]);
+void Xoavitricu (int toadox[] , int toadoy[]);
+
+//SV3 Sa
+void Dichuyeran(int toadox[],int toadoy[], int x , int y,int &toadothucanx,int &toadothucany);
+void them(int a[] , int x );
+void Xoa (int a[] , int vt);
+
+//SV4- Ảnh
+bool RanChamTuong (int toadox , int toadoy );
+bool Ranchamduoi (int toadox[], int toadoy[] );
+bool checkdieukienGameover (int toadox[] , int toadoy[] );
+
+//SV5 - Phúc
+void KhoiTaoToaDoThucAn(int toadoranx[], int toadorany[],int &toadothucanx, int &toadothucany);// tao thuc an
+bool checkRanDeLenThucAn (int toadoranx[], int toadorany[],int toadothucanx, int toadothucany);
+bool XuLyRanAnThucAn (int toadoranx, int toadorany,int toadothucanx, int toadothucany);
+// cải tiến phía sau
 int main()
 {
 
