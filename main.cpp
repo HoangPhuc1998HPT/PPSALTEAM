919d9c8c5886cb672758911a0bbc9ede4ccfce18
UIT-22730092 <22730092@ms.uit.edu.vn>
2023-07-29 10:18:20 +0700

    moving snank by keyboard

--- main.cpp
+++ main.cpp
@@ -7,12 +7,13 @@
 #include <time.h>
 #define Max 99
 #include <windows.h>
-#include <colordlg.h>
+//#include <colordlg.h>
 #include "mylibsnake.h"
 
 
 using namespace std;
 int lenghtsnake = 6 ;
+int sl = 4;
 
 //SV1 vẽ tường + khai báo main - Lâm
 void VeTuongTren();
@@ -25,6 +26,11 @@ void VeTuong();
 void Khoitaotoadoran(int toadox[], int toadoy[]);
 void VeRan (int toadox[] , int toadoy[]);
 void Xoavitricu (int toadox[] , int toadoy[]);
+
+//SV3- Sa
+void XuLyRan(int toadox[], int toadoy[], int x, int y);
+void them(int a[], int x);
+void xoa(int a[], int vt);
 
 //SV4- Ảnh
 bool RanChamTuong (int toadox , int toadoy );
@@ -44,7 +50,58 @@ int main()
 
 // Khởi tạo tọa độ rắn + vẽ
         Khoitaotoadoran(toadox , toadoy) ;
-        VeRan(toadox, toadoy) ;
+        VeRan(toadox, toadoy);
+ //Sa
+        int x = 50 , y = 14;
+        int check = 2; // mac dinh ran di chuyen qua phai
+        while(true)
+        {
+            Xoavitricu(toadox,toadoy);
+            if(_kbhit())
+            {
+                char kitu = _getch();
+                if(kitu == -32)
+                {
+                    kitu = _getch();
+                    if(kitu == 72 && check != 0) //up
+                    {
+                        check =1;
+                    }
+                    else if(kitu == 80 && check != 1) //down
+                    {
+                        check = 0;
+                    }
+                    else if(kitu == 77 && check != 3) //right
+                    {
+                        check = 2;
+                    }
+                    else if(kitu == 75 && check != 2) //left
+                    {
+                        check = 3;
+                    }
+                }
+
+            }
+
+            if(check == 0)
+            {
+                y++;
+            }
+            else if(check == 1)
+            {
+                y--;
+            }
+            else if(check == 2)
+            {
+                x++;
+            }
+            else if(check == 3)
+            {
+                x--;
+            }
+            XuLyRan(toadox, toadoy, x, y);
+            Sleep(300);
+        }
 
     return 0;
 }
@@ -145,7 +202,40 @@ void Xoavitricu (int toadox[] , int toadoy[]) {
         }
     }
 
-// ham xu  ly di chuyen cua ran
+// ham xu  ly di chuyen cua ran - SA
+void XuLyRan(int toadox[], int toadoy[], int x, int y)
+{
+    //them toa do moi vao dau mang
+    them(toadox,x);
+    them(toadoy,y);
+
+    //xoa toa do cuoi mang
+    xoa(toadox, sl - 1);
+    xoa(toadoy, sl - 1);
+
+    //ve ran
+    VeRan(toadox, toadoy);
+}
+
+void them(int a[], int x)
+{
+    for(int i = sl; i> 0; i--)
+    {
+        a[i] = a[i -1];
+    }
+    a[0] = x;
+    sl++;
+}
+
+void xoa(int a[], int vt)
+{
+    for(int i = vt; i< sl; i++)
+    {
+        a[i] = a[i + 1];
+    }
+    sl--;
+}
+
 //xu ly ran di chuyen
 //SV4-Ảnh
 bool RanChamTuong (int toadox , int toadoy ) //xuat toa do ran hien tai
