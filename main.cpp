392c2f5a1cc60bf215e3eda4d182ce6df73ef05d
UIT-22730091 <139626775+HoangPhuc1998HPT@users.noreply.github.com>
2023-07-29 22:09:37 +0700

    hàm xử lý thức ăn
    tich hợp hàm tạo thức ăn + tích hợp vào hàm xử lý rắn để rắn khi ăn thức ăn + thêm 1 đốt --> xuất hiện tình trạng ăn 1 cục tăng 2 điểm. hiện chưa biết lý do tại đâu. tạm cho thông số lenghtsnake -- để giảm lại.
    Có xử lý 1 lỗi phương thức hiểu lầm giữ sl =4 và lenghtsnake - 6 trong gọi hàm khi đã khai báo biến toàn cục

--- main.cpp
+++ main.cpp
@@ -13,7 +13,9 @@
 
 using namespace std;
 int lenghtsnake = 6 ;
-int sl = 4;
+//int sl = 4;
+int tocdochay = 400 ;
+int DiemsoHT = 0 ;
 
 //SV1 vẽ tường + khai báo main - Lâm
 void VeTuongTren();
@@ -28,14 +30,24 @@ void VeRan (int toadox[] , int toadoy[]);
 void Xoavitricu (int toadox[] , int toadoy[]);
 
 //SV3- Sa
-void XuLyRan(int toadox[], int toadoy[], int x, int y);
+void XuLyRan(int toadox[],int toadoy[], int x , int y,int &toadothucanx,int &toadothucany);
 void them(int a[], int x);
-void xoa(int a[], int vt);
+void Xoa (int a[] , int vt);
 
 //SV4- Ảnh
 bool RanChamTuong (int toadox , int toadoy );
 bool Ranchamduoi (int toadox[], int toadoy[] );
 bool checkdieukienGameover (int toadox[] , int toadoy[] );
+
+//SV5 - Phúc
+void KhoiTaoToaDoThucAn(int toadoranx[], int toadorany[],int &toadothucanx, int &toadothucany);// tao thuc an
+bool checkRanDeLenThucAn (int toadoranx[], int toadorany[],int toadothucanx, int toadothucany);
+bool XuLyRanAnThucAn (int toadoranx, int toadorany,int toadothucanx, int toadothucany);
+// cải tiến phía sau
+// vẽ ô ghi điểm
+// Ghi diem so
+void DiemSo (int &Sodiemoday);
+
 
 int main()
 {
@@ -51,45 +63,39 @@ int main()
 // Khởi tạo tọa độ rắn + vẽ
         Khoitaotoadoran(toadox , toadoy) ;
         VeRan(toadox, toadoy);
+
+// hởi tạo tọa độ va kiem tra
+        srand(time(NULL)) ; // lấy giá trị theo thời gian thực
+        int toadothucanx = 0, toadothucany = 0  ;
+        KhoiTaoToaDoThucAn(toadox, toadoy , toadothucanx, toadothucany ); // tọa độ được khởi tạo ngẫu nhiên
+
  //Sa
         int x = 50 , y = 14;
         int check = 2; // mac dinh ran di chuyen qua phai
-        while(true)
+        while(GameOver == false)
         {
             Xoavitricu(toadox,toadoy);
-            if(_kbhit())
-            {
-                char kitu = _getch();
-                if(kitu == -32)
-                {
-                    kitu = _getch();
-                    if(kitu == 72 && check != 0) //up
-                    {
-                        check =1;
-                    }
-                    else if(kitu == 80 && check != 1) //down
-                    {
-                        check = 0;
-                    }
-                    else if(kitu == 77 && check != 3) //right
-                    {
-                        check = 2;
-                    }
-                    else if(kitu == 75 && check != 2) //left
-                    {
-                        check = 3;
-                    }
-                }
+                if (kbhit()){
+            char kitu = getch();
 
-            }
+            if ((kitu == 'a'|| kitu ==75 )&& check!= 2) { //qua trai
+                    check = 3;
+            } else if ((kitu == 'w' || kitu == 72)&& check!= 1) {//di len
+                check = 0;
+            } else if ((kitu == 'd'|| kitu == 77) && check!= 3) {//qua phai
+                check = 2;
+           } else if ((kitu == 's' || kitu == 80)&& check!= 0) {//di xuong
+               check = 1;
+           }
+}
 
             if(check == 0)
             {
-                y++;
+                y--;
             }
             else if(check == 1)
             {
-                y--;
+                y++;
             }
             else if(check == 2)
             {
@@ -99,9 +105,22 @@ int main()
             {
                 x--;
             }
-            XuLyRan(toadox, toadoy, x, y);
-            Sleep(300);
+
+         //   XuLyRan(toadox, toadoy, x, y);
+
+            //xử lý cho ran chay
+        XuLyRan (toadox, toadoy, x , y,toadothucanx,toadothucany) ;
+
+
+// Xoa(toadox,toadoy)
+        GameOver = checkdieukienGameover ( toadox , toadoy );
+        this_thread::sleep_for(chrono::milliseconds(tocdochay)) ;
+
         }
+
+
+
+
 
     return 0;
 }
@@ -203,37 +222,56 @@ void Xoavitricu (int toadox[] , int toadoy[]) {
     }
 
 // ham xu  ly di chuyen cua ran - SA
-void XuLyRan(int toadox[], int toadoy[], int x, int y)
+void XuLyRan(int toadox[],int toadoy[], int x , int y,int &toadothucanx,int &toadothucany)
 {
-    //them toa do moi vao dau mang
-    them(toadox,x);
-    them(toadoy,y);
+    // them toa do vao dau mang
 
-    //xoa toa do cuoi mang
-    xoa(toadox, sl - 1);
-    xoa(toadoy, sl - 1);
+        them(toadox, x);
+        them(toadoy, y);
+    if ((XuLyRanAnThucAn(toadox[0], toadoy[0], toadothucanx,toadothucany)) == false )
+    {
+    // xoa toa do cuoi mang
+        Xoa(toadox , lenghtsnake - 1);
+        Xoa(toadoy, lenghtsnake - 1);
 
-    //ve ran
-    VeRan(toadox, toadoy);
+    //KhoiTaoToaDoThucAn(toadox, toadoy,toadothucanx, toadothucany);
+
+    } else {
+     //bo qua xoa toa do cuoi mang
+        lenghtsnake-- ;
+         KhoiTaoToaDoThucAn(toadox, toadoy,toadothucanx, toadothucany);
+
+        // dieu chinh toc do ran chay sau khi rắn ăn quả
+
+          tocdochay = tocdochay - 10 ; //giới hạn khi tocdochay = 0
+          // sau mỗi lần ăn thức ăn rắn sẽ tăng tốc độ lên 10đ
+
+
+
+    }
+    // ve ran
+     VeRan(toadox, toadoy);
 }
 
-void them(int a[], int x)
-{
-    for(int i = sl; i> 0; i--)
-    {
-        a[i] = a[i -1];
-    }
-    a[0] = x;
-    sl++;
-}
 
-void xoa(int a[], int vt)
+void them(int a[] , int x )
 {
-    for(int i = vt; i< sl; i++)
+    for (int i = lenghtsnake ; i > 0 ; i-- )
     {
-        a[i] = a[i + 1];
+        a[i] = a[i - 1] ;
     }
-    sl--;
+    a[0] = x ;
+    lenghtsnake++;
+
+}
+void Xoa (int a[] , int vt)
+{
+    for (int i = vt ; i < lenghtsnake ; i++ )
+    {
+        a[i] = a[i+1] ;
+
+    }
+    lenghtsnake-- ;
 }
 
 //xu ly ran di chuyen
@@ -279,4 +317,41 @@ bool Ranchamduoi (int toadox[], int toadoy[] )
         return false ;
      }
 
+bool checkRanDeLenThucAn(int toadoranx[], int toadorany[],int toadothucanx, int toadothucany)
+{
+        for (int i = 0 ; i < lenghtsnake ; i++ )
+        {
+            if ((toadothucanx == toadoranx[i] )&&( toadothucany == toadorany[i]))
+            {
+                return true ;// ran de len thuc an
+            }
+        }
+    return false ;// ran khong de len qua
+   // KhoiTaoToaDoThucAn(toadothucanx,toadothucany)
+}
+
+
+void KhoiTaoToaDoThucAn(int toadoranx[], int toadorany[],int &toadothucanx, int &toadothucany)
+{
+    do
+    {
+    toadothucanx = rand ()% (99 - 11 + 1 ) + 11 ; //x >10 - x =11
+    toadothucany = rand ()%( 27 - 2 + 1) + 2 ; // y > 1 -y = 2
+    } while (checkRanDeLenThucAn(toadoranx,toadorany,toadothucanx,toadothucany)== true) ;
+
+    gotoXY(toadothucanx,toadothucany) ;
+
+    cout<< "@" ;
+    }
+
+// neu toa do ran = toa do thuc an ran ++
+bool XuLyRanAnThucAn (int toadoranx, int toadorany,int toadothucanx, int toadothucany) // khi ran an qua
+{
+        if ((toadoranx == toadothucanx) && (toadorany == toadothucany ))
+        {
+            return true ;
+
+        }
+    return false ;
+}
 
